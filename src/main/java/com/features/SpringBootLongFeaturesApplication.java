17544095d02ac4da91501371dc780321752a4680
Juan Carlos Coronado Velasquez <jc@Steren-Audifonos-Bt.local>
2023-06-30 10:06:49 -0600

    primer commit

---
+++ src/main/java/com/features/SpringBootLongFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLongFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLongFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/features/SpringBootLongFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLongFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
